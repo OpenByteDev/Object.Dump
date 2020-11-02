deb25e80a45b339bba7afcce12202a6fb1aeea9d
OpenByte <development.openbyte@gmail.com>
2020-11-02 21:34:20 +0100

    Initial commit

---
+++ ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ObjectDump.Extensions {
+    public static class ObjectExtensions {
+
+        public static void DumpToConsole(this object obj) => Console.WriteLine(ObjectDumper.Dump(obj));
+        public static void DumpToError(this object obj) => Console.Error.WriteLine(ObjectDumper.Dump(obj));
+        public static void DumpToTrace(this object obj) => Trace.WriteLine(ObjectDumper.Dump(obj));
+        public static string Dump(this object obj) => ObjectDumper.Dump(obj);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Object.Dump
+
+[![nuget badge](https://badgen.net/nuget/v/object.dump)](https://www.nuget.org/packages/WinEventHook/0.2.0)
+[![Unlicense](https://img.shields.io/github/license/OpenByteDev/Object.Dump)](./LICENSE)
+
+Provides simple `.Dump()` methods to easily print objects in full to the console.
+
+The core `ObjectDumper` was taken from https://stackoverflow.com/a/10478008/6304917.
